5971efe82d0f3252bf6b5d36415398c9aec6edd1
globalnebula <kunalachintya@gmail.com>
2023-11-26 20:38:10 +0530

    added UI using TK tools

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,12 @@
-import re
+import tkinter as tk
+from tkinter import scrolledtext
 import json
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import make_pipeline
 import random
-from googlesearch import search
+import re
 
 data = {
     'text': [
@@ -24,7 +24,7 @@ data = {
         'Amazed by the unexpected', 'Reflecting on past achievements', 'Energetic and ready for action', 'Grumpy and in need of a break',
         'Satisfied with a job well done', 'Curious about the mysteries of the universe',
         'Overwhelmed with joy', 'Feeling on top of the world', 'Anxious about the unknown', 'Curious about the world', 'Sad but hopeful',
-        'Excited and curious', 'Feeling content and curious', 'Angry and frustrated', 'Joyful despite challenges', 'Curious about the future','I am gay'
+        'Excited and curious', 'Feeling content and curious', 'Angry and frustrated', 'Joyful despite challenges', 'Curious about the future', 'I am gay'
     ],
     'emotion': [
         'joy', 'sadness', 'anger', 'fear', 'surprise', 'disgust',
@@ -34,7 +34,7 @@ data = {
         'determination', 'loneliness', 'optimism', 'skepticism',
         'joy', 'sadness', 'anger', 'joy', 'sadness', 'joy', 'anger', 'joy', 'neutral', 'joy',
         'joy', 'joy', 'contentment', 'joy', 'surprise', 'contentment', 'anger', 'joy', 'neutral', 'curiosity',
-        'joy', 'curiosity', 'anxiety', 'curiosity', 'sadness', 'joy', 'contentment', 'anger', 'joy', 'curiosity','sadness'
+        'joy', 'curiosity', 'anxiety', 'curiosity', 'sadness', 'joy', 'contentment', 'anger', 'joy', 'curiosity', 'sadness'
     ]
 }
 
@@ -46,80 +46,92 @@ farewells = [
     'Goodbye', 'Bye', 'Farewell', 'See you later', 'Take care', 'Adios'
 ]
 
-def classify_tone(text, model, vectorizer):
-    # Transform text using the vectorizer
-    text_vectorized = vectorizer.transform([text])
-    
-    # Predict the emotion using the trained model
-    emotion = model.predict(text_vectorized)[0]
-    return emotion
+class ChatbotUI:
+    def __init__(self, model, vectorizer):
+        self.model = model
+        self.vectorizer = vectorizer
+        self.responses = self.load_responses()
 
+        self.window = tk.Tk()
+        self.window.title("Chatbot UI")
 
-def load_responses():
-    # Load response templates from a JSON file
-    with open('data.json', 'r', encoding="utf-8") as file:
-        responses = json.load(file)
-    return responses
+        self.create_widgets()
 
+    def load_responses(self):
+        with open('data.json', 'r', encoding="utf-8") as file:
+            responses = json.load(file)
+        return responses
 
-def get_remedies(emotion):
-    # Define remedies based on emotion
-    remedies = {
-        'joy': ['Take a walk in nature', 'Listen to uplifting music', 'Call a friend and share your joy'],
-        'sadness': ['Reach out to a friend or family member', 'Engage in activities you enjoy', 'Consider talking to a professional'],
-        'anger': ['Practice deep breathing exercises', 'Take a break to cool off', 'Express your feelings through writing'],
-        'neutral': ['Take a moment to relax', 'Engage in a hobby', 'Plan something enjoyable for yourself'],
-        'curiosity': ['Explore a new topic or hobby', 'Read an interesting article', 'Watch a documentary'],
-        'anxiety': ['Practice mindfulness meditation', 'Focus on your breath', 'Create a calming routine']
-    }
+    def create_widgets(self):
+        self.chat_history = scrolledtext.ScrolledText(self.window, width=50, height=20, wrap=tk.WORD)
+        self.chat_history.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
-    return remedies.get(emotion, [])
+        self.user_input_label = tk.Label(self.window, text="User Input:")
+        self.user_input_label.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W)
 
-def chatbot_response(user_input, responses, model, vectorizer):
-    # Check for greetings
-    if any(greeting in user_input.lower() for greeting in greetings):
-        return "Hello! How can I assist you today?"
+        self.user_input_entry = tk.Entry(self.window, width=40)
+        self.user_input_entry.grid(row=1, column=1, padx=10, pady=5, sticky=tk.W)
 
-    # Check for farewells
-    if any(farewell in user_input.lower() for farewell in farewells):
-        return "Goodbye! Have a great day!"
+        self.send_button = tk.Button(self.window, text="Send", command=self.send_message)
+        self.send_button.grid(row=2, column=1, padx=10, pady=5, sticky=tk.E)
 
-    # Transform the user input using the vectorizer
-    user_input_vectorized = vectorizer.transform([user_input])
+        self.quit_button = tk.Button(self.window, text="Quit", command=self.window.quit)
+        self.quit_button.grid(row=2, column=0, padx=10, pady=5, sticky=tk.W)
 
-    # Predict the emotion using the trained model
-    emotion = model.predict(user_input_vectorized)[0]
+    def send_message(self):
+        user_input = self.user_input_entry.get()
+        self.user_input_entry.delete(0, tk.END)
 
-    if emotion in responses:
-        # Randomly select one response for the predicted emotion
-        emotion_responses = responses[emotion]
-        selected_response = random.choice(emotion_responses)
+        response = self.chatbot_response(user_input)
+        self.update_chat_history(f"You: {user_input}\nBot: {response}")
 
-        # Get remedies for the emotion
-        remedies = get_remedies(emotion)
-        if remedies:
-            selected_response += f"\n\nRemedies for {emotion.capitalize()}: {', '.join(remedies)}"
-
-        return selected_response
-    else:
-        return "I'm not sure how to respond to that. Can you provide more details?"
-
-def start_conversation(responses, model, vectorizer):
-    print("Bot: Hello! How can I assist you today?")
-    while True:
-        user_input = input("You: ")
-
-        # Check for farewell to end the conversation
+    def chatbot_response(self, user_input):
         if any(farewell in user_input.lower() for farewell in farewells):
-            print("Bot: Goodbye! Have a great day!")
-            break
+            return "Goodbye! Have a great day!"
 
-        response = chatbot_response(user_input, responses, model, vectorizer)
-        print(f"Bot: {response}")
+        matched_pattern = self.match_pattern(user_input)
+        if matched_pattern:
+            return matched_pattern
 
+
+        user_input_vectorized = self.vectorizer.transform([user_input])
+
+
+        emotion = self.model.predict(user_input_vectorized)[0]
+
+        if emotion in self.responses:
+            emotion_responses = self.responses[emotion]
+            selected_response = random.choice(emotion_responses)
+
+            remedies = self.get_remedies(emotion)
+            if remedies:
+                selected_response += f"\n\nRemedies for {emotion.capitalize()}:"
+                for remedy in remedies:
+                    selected_response += f"\n- {remedy}"
+
+            return selected_response
+        else:
+            return "I'm not sure how to respond to that. Can you provide more details?"
+
+    def match_pattern(self, user_input):
+        for pattern, response in self.responses.get("patterns", {}).items():
+            if re.search(fr'\b{pattern}\b', user_input, re.IGNORECASE):
+                return response
+        return None
+
+
+
+    def get_remedies(self, emotion):
+        return self.responses.get("remedies", {}).get(emotion, [])
+
+    def update_chat_history(self, message):
+        self.chat_history.insert(tk.END, f"{message}\n\n")
+        self.chat_history.yview(tk.END)
+
+    def start(self):
+        self.window.mainloop()
 
 def main():
-    # Data processing and model training
     df = pd.DataFrame(data)
 
     greeting_data = [{'text': f'{g} how are you feeling?', 'emotion': 'neutral'} for g in greetings]
@@ -134,8 +146,9 @@ def main():
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train_vectors, y_train)
 
-    responses = load_responses()
-    start_conversation(responses, model, vectorizer)
+    chatbot_ui = ChatbotUI(model, vectorizer)
+    chatbot_ui.start()
 
 if __name__ == "__main__":
     main()
+
